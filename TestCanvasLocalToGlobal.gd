ea76031f69e092b21145f1ff77f8b7c695da880f
Kyle Luce <razzlegames@gmail.com>
2020-03-13 13:51:55 -0700

    Rename to make_local_canvas_position_global

--- TestCanvasLocalToGlobal.gd
+++ TestCanvasLocalToGlobal.gd
@@ -2,5 +2,5 @@ extends Node2D
 
 func _ready():
 		var testLocal = make_canvas_position_local(Vector2(30, 45))
-		var testScreen = make_local_canvas_position(testLocal)
+		var testScreen = make_local_canvas_position_global(testLocal)
 		print(str(testScreen))
